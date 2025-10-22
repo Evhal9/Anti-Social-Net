d2a493203add128cf05f51947ab9f503977d49f5
Mely Salerno <melysalernoflores@gmail.com>
2025-10-22 13:44:46 -0300

    Empezando login, agrgando logo unahur

---
+++ src/pages/Login.tsx
@@ -0,0 +1,23 @@
+function Login() {
+    return <>
+    <section>
+        <div>
+            <div>
+                <button></button>
+                <button></button>
+            </div>
+            <div>
+                <div>
+                    <h3>Iniciar sesion</h3>
+                    
+                </div>
+                <div>
+                    <h3> Registrarse </h3>
+                </div>
+            </div>
+        </div>
+    </section>
+    </>
+}
+
+export default Login
